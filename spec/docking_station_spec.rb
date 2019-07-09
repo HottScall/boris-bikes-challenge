c5848d1be11c91a21e15436dd7019a0479b3cf5e
Scott <Scotthall1983@hotmail.com>
2019-07-09 16:43:11 +0100

    Added a capacity of 20 to the docks as per user story, wrote tests with a 20 bike limit, added an array to store multiple bikes, refactored new code to ensure tests passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,18 +12,26 @@ describe DockingStation do
 
     it 'returns an error when no bike is docked ' do
       expect { subject.release_bike }.to raise_error "No bikes available"
-    end 
+    end
   end
 
-  it 'docks a bike' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe "#dock" do
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq [bike]
+    end
+
+    it 'returns an error when a dock is full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error "Docking Station full"
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to eq [bike]
+    end
   end
 
 end
